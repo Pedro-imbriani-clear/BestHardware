75f3ec522b75b9cfc34430b133d9a677793a766d
Pedro-imbriani-clear <pedro.imbriani.clear@gmail.br>
2023-06-07 12:59:57 -0300

    addition of CSS and best sellers in the home

--- besthardware-front/src/view/about.jsx
+++ besthardware-front/src/view/about.jsx
@@ -3,13 +3,18 @@ import '../component/about.css'
 import Nav from '../component/nav'
 
 export default props => {
-    return(
-        <div className='body'>
-             <Nav/> 
-             <h1 className='p'>Somos uma  a melhor empresa de hardware</h1>
-             <p className='p'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi reprehenderit mollitia magni, aperiam obcaecati! Fuga ratione non dolorem enim eveniet voluptates illo tempore qui, sapiente aspernatur fugiat saepe exercitationem!</p>
+  return (
+    <div>
+      <Nav />
+      <div className='body'>
 
+        <h1 className='p'>Somos uma  a melhor empresa de hardware</h1>
+        <p className='p'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi reprehenderit mollitia magni, aperiam obcaecati! Fuga ratione non dolorem enim eveniet voluptates illo tempore qui, sapiente aspernatur fugiat saepe exercitationem!</p>
+        <div className='img'>
+          <img url="../../public/img/quality.png" />
         </div>
-  
-    )
+      </div>
+    </div>
+
+  )
 }
--- besthardware-front/src/view/catalog.jsx
+++ besthardware-front/src/view/catalog.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Nav from '../component/nav'
+import '../component/home.css'
 
 
 export default props => {
     return(
-        <div>
+        <div className='full'>
             <Nav/>
 
             
